55cdc680a3b43c4b0f575992606d49c4d28434b2
Divya Kamath <dimallya@in.ibm.com>
2020-08-05 12:47:33 +0000

    output vars defined

--- Create_T2_Micro_EC2/outputs.tf
+++ Create_T2_Micro_EC2/outputs.tf
@@ -4,3 +4,14 @@
 ##
 #####################################################################
 
+output "vm_public_ip" {
+  value = "${aws_instance.aws_instance.public_ip}"
+}
+
+output "sample_message" {
+  value = "This is just a sample message"
+}
+
+output "vm_ssh_private_key" {
+  value = "${tls_private_key.ssh.private_key_pem}"
+}
