2bf31d26a8323db2af17c896ad15e5109151b6ed
Divya Kamath <dimallya@in.ibm.com>
2020-08-05 12:09:28 +0000

    updates

---
+++ Create_T2_Micro_EC2/README.md
@@ -0,0 +1,8 @@
+## Create_T2_Micro_EC2 infrastructure
+
+Created 8/5/20 by mcmadmin. for Create_T2_Micro_EC2
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ Create_T2_Micro_EC2/variables.tf
@@ -0,0 +1,32 @@
+#####################################################################
+##
+##      Created 8/5/20 by mcmadmin. for Create_T2_Micro_EC2
+##
+#####################################################################
+
+variable "aws_instance_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+  default = "t2.micro"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_key_pair_name" {
+  type = "string"
+  description = "Generated"
+}
+
